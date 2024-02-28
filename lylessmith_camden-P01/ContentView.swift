d781c7a4d57b032816f47677ee853ea478883310
clylessmith <camdenlylessmith@gmail.com>
2024-02-27 21:05:17 -0700

    Initial Commit

---
+++ lylessmith_camden-P01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lylessmith_camden-P01
+//
+//  Created by Camden Lyles-Smith on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lylessmith_camden-P01/lylessmith_camden_P01App.swift
@@ -0,0 +1,17 @@
+//
+//  lylessmith_camden_P01App.swift
+//  lylessmith_camden-P01
+//
+//  Created by Camden Lyles-Smith on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct lylessmith_camden_P01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
